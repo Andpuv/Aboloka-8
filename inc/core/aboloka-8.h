d9e51d241b34b0aa59c0186a55d5c391a9a79864
Andpuv <andpuv.dev@gmail.com>
2024-11-10 10:51:02 +0100

    Adding core single header

---
+++ inc/core/aboloka-8.h
@@ -0,0 +1,8 @@
+# ifndef __CORE_ABOLOKA_8_H__
+#   define __CORE_ABOLOKA_8_H__
+
+#   include "aboloka-8-api.h"
+#   include "aboloka-8-ram.h"
+#   include "aboloka-8-cpu.h"
+
+# endif /* __CORE_ABOLOKA_8_H__ */
